a70316f3087b244cd3a80b6a57a3e4eb46fe6412
Cesara02 <90842096+Cesara02@users.noreply.github.com>
2021-12-06 19:48:59 +0100

    Add files via upload

---
+++ C/TP2/Exercice1/Exercice1/Exercice1.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+int multiple(int value, int n) {
+	return value % n == 0;
+}
+
+int main() {
+
+	int n;
+	int tab[3] = {2, 3 6}
+
+	printf("Entrez une valeur ");
+	scanf("%d" &n);
+
+
+}
+
+/*
+
+Ecrire deux fonctions qui retourne une valeur entière permettant de savoir si l’entier passé en
+argument est multiple de deux et multiple de trois pour la seconde fonction.
+Utiliser ces deux fonctions dans un petit programme qui lit un nombre entier et qui précise s’il
+est pair, multiple de 3 et/ou multiple de 6.
+
+*/
---
+++ C/TP2/Exercice3/Exercice3/Exercice3.cpp
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream> 
+#include <stdio.h>
+
+
+void transformation(char *chaine) {
+	int i = 0;
+
+	for (i = 0; chaine[i] != '\0'; i++) {
+		if (chaine[i] >= 'A' && chaine[i] <= 'Z')
+			chaine[i] = chaine[i] - 'A' + 'a';
+	}
+
+	printf("Voici la chaine convertie : %s .\n", chaine);
+}
+
+int main(int argc, char *argv[]) {
+
+	char str[100];
+
+	printf("Entrez la chaine a convertir : ");
+	scanf("%c", &str);
+
+	transformation(str);
+
+	return 0;
+}
